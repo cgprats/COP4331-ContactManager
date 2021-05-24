a3142fcc06f1b550041c874df109b3f3a18e3771
snowbowls <61566230+snowbowls@users.noreply.github.com>
2021-05-24 10:32:26 -0400

    Starting Sign Up

---
+++ LAMPAPI/SignUp.php
@@ -0,0 +1,51 @@
+
+<?php
+
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = $inData["firstname"];
+	$lastName = $inData["lastname"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT id,firstname,lastname into users WHERE Login=? AND Password =?");
+		$stmt->bind_param("ss", $firstName, $lastName);
+		
+		$stmt->execute();
+		
+		$stmt->close();
+		$conn->close();
+		
+		returnWithError("");
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
