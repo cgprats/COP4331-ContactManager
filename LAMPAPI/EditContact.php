0728cd825d355690cee95f7e501039584f6146ad
snowbowls <61566230+snowbowls@users.noreply.github.com>
2021-05-24 15:17:37 -0400

    Added Edit API

---
+++ LAMPAPI/EditContact.php
@@ -0,0 +1,38 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+
+	$conn = new mysqli("localhost", "admin", "plsletM3in", "smallproject"); 
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE contacts SET firstname=?, lastname=?, email=?, phone=? WHERE fooid=?");
+		$stmt->bind_param("ssssi", $inData["firstname"], $inData["lastname"], $inData["email"], $inData["phone"], $inData["fooid"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError($inData["ContactsID"]);
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
