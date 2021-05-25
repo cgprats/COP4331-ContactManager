6390439599fb56fefc846b2bbfc47d77999a13c7
snowbowls <61566230+snowbowls@users.noreply.github.com>
2021-05-25 14:08:51 -0400

    Updated to search both first and last

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -12,9 +12,9 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("select * from contacts where firstname like ? and fooid=?");
+		$stmt = $conn->prepare("select * from contacts where firstname like ? or lastname like ? and fooid=?");
 		$search = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $search, $inData["fooid"]);
+		$stmt->bind_param("sss", $search, $search, $inData["fooid"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
