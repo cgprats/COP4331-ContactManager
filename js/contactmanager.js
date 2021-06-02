7b9e24e49c41bfb1a4e35e36c12c76e618692c02
James <nah>
2021-06-02 13:50:18 +0000

    Add Webserver JS

--- js/contactmanager.js
+++ js/contactmanager.js
@@ -93,7 +93,158 @@ function doRegister() {
 	}
 }
 
+function addContact() {
+	var newFirstName = document.getElementById("contactFirstName").value;
+	var newLastName = document.getElementById("contactLastName").value;
+	var newEmail = document.getElementById("contactEmail").value;
+	var newPhone = document.getElementById("contactPhone").value;
+	
+	if (newFirstName == "" || newLastName == "" || newEmail == "" || newPhone == "")
+	{
+		document.getElementById("contactAddResult").innerHTML = "All fields are required.";
+		return;
+	}
+	
+	document.getElementById("contactAddResult").innerHTML = "";
+	
+	var jsonPayload = '{"firstname" : "' + newFirstName + '", "lastname" : "' + newLastName + '", "email" : "' + newEmail + '", "phone" : "' + newPhone + '", "fooid" : ' + userId + '}';
+	var url = urlBase + '/LAMPAPI/AddContact.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactAddResult").innerHTML = err.message;
+	}
+}
+
+function deleteContact() {
+	var deleteId = document.getElementById("contactDeleteID").value;
+	document.getElementById("contactDeleteResult").innerHTML = "";
+	
+	var jsonPayload = '{"id" : ' + deleteId + '}';
+	var url = urlBase + '/LAMPAPI/DeleteContact.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactDeleteResult").innerHTML = "Contact has been deleted";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactDeleteResult").innerHTML = err.message;
+	}
+}
+
 function doSearch() {
+	var srch = document.getElementById("searchText").value;
+	document.getElementById("searchResult").innerHTML = "";
+	
+	var contactList = "";
+	
+	var jsonPayload = '{"search" : "' + srch + '", "fooid" : ' + userId + '}';
+	var url = urlBase + '/LAMPAPI/SearchContacts.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("searchResult").innerHTML = "Contact(s) has been retrieved";
+				var jsonObject = JSON.parse(xhr.responseText);
+				
+				if (jsonObject.error != "")
+				{
+					document.getElementById("searchResult").innerHTML = jsonObject.error;
+					document.getElementsByTagName("p")[0].innerHTML = "";
+					return;
+				}
+				
+				for (var i = 0; i < jsonObject.results.length; i++)
+				{
+					contactList += "Name: " + jsonObject.results[i].lastname + ", " + jsonObject.results[i].firstname + "<br>";
+					contactList += "Email: " + jsonObject.results[i].email + "<br>";
+					contactList += "Phone: " + jsonObject.results[i].phone + "<br>";
+					contactList += "ID: " + jsonObject.results[i].id + "<br>";
+					
+					if (i < jsonObject.results.length - 1)
+					{
+						contactList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("searchResult").innerHTML = err.message;
+	}
+}
+
+function editContact() {
+	var editId = document.getElementById("contactEditID").value;
+	var newFirstName = document.getElementById("contactEditFirstName").value;
+	var newLastName = document.getElementById("contactEditLastName").value;
+	var newEmail = document.getElementById("contactEditEmail").value;
+	var newPhone = document.getElementById("contactEditPhone").value;
+	
+	if (newFirstName == "" || newLastName == "" || newEmail == "" || newPhone == "")
+	{
+		document.getElementById("contactEditResult").innerHTML = "All fields are required.";
+		return;
+	}
+	
+	document.getElementById("contactEditResult").innerHTML = "";
+	
+	var jsonPayload = '{"firstname" : "' + newFirstName + '", "lastname" : "' + newLastName + '", "email" : "' + newEmail + '", "phone" : "' + newPhone + '", "id" : ' + editId + '}';
+	var url = urlBase + '/LAMPAPI/EditContact.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactEditResult").innerHTML = "Contact has been edited";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactEditResult").innerHTML = err.message;
+	}
 }
 
 function saveCookie() {
